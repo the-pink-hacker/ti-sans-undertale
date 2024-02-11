f948a5fb78df8592dc18cb7965da743a37872a4d
Ryan Garrett <ryan@thepinkhacker.com>
2024-02-11 16:40:39 -0500

    Add rotation support to the asset builder

--- asset-builder/src/sprite.rs
+++ asset-builder/src/sprite.rs
@@ -6,12 +6,43 @@ use std::{
 
 use crate::PointerTable;
 use anyhow::{bail, Context};
+use image::Rgb;
+use imageproc::geometric_transformations::Interpolation;
 use linked_hash_map::LinkedHashMap;
 use serde::Deserialize;
+use serde_valid::Validate;
+
+#[derive(Debug, Deserialize, Validate, Clone)]
+pub struct ExpandedSprite {
+    pub path: PathBuf,
+
+    #[validate(minimum = -360.0)]
+    #[validate(maximum = 360.0)]
+    pub rotation: Option<f32>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+pub enum Sprite {
+    Path(PathBuf),
+    Expanded(ExpandedSprite),
+}
+
+impl Sprite {
+    pub fn to_expanded(&self) -> ExpandedSprite {
+        match self {
+            Self::Path(path) => ExpandedSprite {
+                path: path.to_path_buf(),
+                rotation: None,
+            },
+            Self::Expanded(sprite) => sprite.clone(),
+        }
+    }
+}
 
 #[derive(Debug, Deserialize)]
 pub struct SpriteMetadata {
-    pub sprites: LinkedHashMap<String, PathBuf>,
+    pub sprites: LinkedHashMap<String, Sprite>,
     pub pointer_table: Option<PointerTable>,
 }
 
@@ -28,7 +59,8 @@ fn compress_color_space(rgb: [u8; 3]) -> String {
     let green = green / 32;
     let blue = (blue / 64) << 3;
     let pixel = red | green | blue;
-    format!("${:x}", pixel)
+
+    format!("${:x}", pixel).to_uppercase()
 }
 
 pub fn generate_sprite(
@@ -39,11 +71,13 @@ pub fn generate_sprite(
 ) -> anyhow::Result<()> {
     let mut output = String::new();
 
-    for (sprite_suffix, sprite_name) in metadata.sprites.iter() {
+    for (sprite_suffix, sprite) in metadata.sprites.iter() {
+        let sprite = sprite.to_expanded();
+
         let source_image_path = sprite_path
             .parent()
             .with_context(|| "Sprite path was empty.")?
-            .join(sprite_name);
+            .join(sprite.path);
 
         {
             let is_png = source_image_path
@@ -62,10 +96,10 @@ pub fn generate_sprite(
             };
         }
 
-        let sprite_png = image::io::Reader::open(source_image_path.clone())?.decode()?;
+        let sprite_data = image::io::Reader::open(source_image_path.clone())?.decode()?;
 
-        let width = sprite_png.width();
-        let height = sprite_png.height();
+        let width = sprite_data.width();
+        let height = sprite_data.height();
 
         output += &format!(
             "\n{}:\n\
@@ -75,15 +109,23 @@ pub fn generate_sprite(
             sprite_suffix, width, height
         );
 
-        let pixels = sprite_png
-            .to_rgb8()
-            .pixels()
-            .map(|pixel| compress_color_space(pixel.0))
-            .collect::<Vec<_>>();
+        let pixels = if let Some(rotation) = sprite.rotation {
+            imageproc::geometric_transformations::rotate_about_center(
+                &sprite_data.to_rgb8(),
+                rotation.to_radians(),
+                Interpolation::Bilinear,
+                Rgb([0, 0, 0]),
+            )
+        } else {
+            sprite_data.to_rgb8()
+        }
+        .pixels()
+        .map(|pixel| compress_color_space(pixel.0))
+        .collect::<Vec<_>>();
 
         output += &pixels
             .chunks_exact(width as usize)
-            .map(|pixle| pixle.join(","))
+            .map(|pixel| pixel.join(","))
             .map(|line| format!("\ndb {}", line))
             .collect::<String>();
     }
