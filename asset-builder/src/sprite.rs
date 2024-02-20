4e988090e352aae3df59498f6b764eb6d11fda3f
Ryan Garrett <ryan@thepinkhacker.com>
2024-02-19 21:53:06 -0500

    Add header and color space options to asset builder

--- asset-builder/src/sprite.rs
+++ asset-builder/src/sprite.rs
@@ -11,13 +11,22 @@ use linked_hash_map::LinkedHashMap;
 use serde::Deserialize;
 use serde_valid::Validate;
 
-#[derive(Debug, Deserialize, Validate, Clone)]
+#[derive(Debug, Deserialize, Clone, Default)]
+#[serde(rename_all = "snake_case")]
+pub enum ColorSpace {
+    #[default]
+    Rgb,
+    Monochrome,
+}
+
+#[derive(Debug, Deserialize, Validate, Clone, Default)]
 pub struct ExpandedSprite {
     pub path: PathBuf,
-
     #[validate(minimum = -360.0)]
     #[validate(maximum = 360.0)]
     pub rotation: Option<f32>,
+    pub color_space: Option<ColorSpace>,
+    pub header: Option<bool>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -32,7 +41,7 @@ impl Sprite {
         match self {
             Self::Path(path) => ExpandedSprite {
                 path: path.to_path_buf(),
-                rotation: None,
+                ..Default::default()
             },
             Self::Expanded(sprite) => sprite.clone(),
         }
@@ -52,6 +61,8 @@ pub struct SpriteMetadata {
     pub sprites: LinkedHashMap<String, Sprite>,
     pub rotation: Option<f32>,
     pub output_type: Option<OutputType>,
+    pub color_space: Option<ColorSpace>,
+    pub header: Option<bool>,
 }
 
 pub fn load_sprite_metadata(
@@ -77,22 +88,56 @@ pub fn rgb_line_to_string(rgb: &[u8]) -> String {
         .join(",")
 }
 
-pub struct RawSprite<'a, T> {
+pub fn monochrome_string(output: &mut String, pixels: &[u8]) {
+    *output += &"\ndb ";
+
+    for pixel in pixels {
+        output.push(if pixel == &0 { '0' } else { '1' });
+    }
+
+    output.push('%');
+}
+
+pub fn unwrap_sprite_option_or<'a, T>(metadata: &'a Option<T>, sprite: &'a Option<T>, value: T) -> T
+where
+    T: Clone + Default,
+{
+    if let Some(sprite) = sprite {
+        sprite.clone()
+    } else {
+        metadata.clone().unwrap_or(value)
+    }
+}
+
+pub fn unwrap_sprite_option<'a, T>(metadata: &'a Option<T>, sprite: &'a Option<T>) -> T
+where
+    T: Clone + Default,
+{
+    if let Some(sprite) = sprite {
+        sprite.clone()
+    } else {
+        metadata.clone().unwrap_or_default()
+    }
+}
+
+pub struct RawSprite<'a> {
     pub sprite_suffix: &'a str,
     pub width: u32,
     pub height: u32,
-    pub pixels: T,
+    pub color_space: ColorSpace,
+    pub pixels: Vec<u8>,
 }
 
-pub fn generate_assembly_sprite(output: &mut String, sprite: RawSprite<Vec<u8>>) {
-    output.push_str(&format!(
-        "\n{}:\n\
-            .width := {}\n\
-            .height := {}\n\
-            db .width, .height",
-        sprite.sprite_suffix, sprite.width, sprite.height
-    ));
+pub fn generate_assembly_sprite_header(output: &mut String, sprite: &RawSprite) {
+    *output += &format!(
+        "\n.width := {}\n\
+         .height := {}\n\
+         db .width, .height",
+        sprite.width, sprite.height
+    );
+}
 
+pub fn generate_assembly_sprite_pixels_rgb(output: &mut String, sprite: &RawSprite) {
     output.push_str(
         &sprite
             .pixels
@@ -103,40 +148,52 @@ pub fn generate_assembly_sprite(output: &mut String, sprite: RawSprite<Vec<u8>>)
     );
 }
 
-pub fn generate_binary_sprite(output: &mut Vec<u8>, mut sprite: RawSprite<Vec<u8>>) {
-    output.push(sprite.width as u8);
-    output.push(sprite.height as u8);
-    output.append(&mut sprite.pixels);
+pub fn generate_assembly_sprite_pixels_monochrome(output: &mut String, sprite: &RawSprite) {
+    let chunks = sprite.pixels.chunks_exact(8);
+    let remainder = chunks.remainder();
+
+    for chunk in chunks {
+        monochrome_string(output, chunk);
+    }
+
+    let remainder_length = remainder.len();
+
+    if remainder_length != 0 {
+        let mut last_byte = [0u8; 8];
+
+        for i in 0..8 - remainder_length {
+            last_byte[i] = remainder[i];
+        }
+
+        monochrome_string(output, &last_byte);
+    }
+}
+
+pub fn check_for_png(path: &PathBuf) -> anyhow::Result<()> {
+    let is_png = path
+        .extension()
+        .with_context(|| format!("Failed to get extension of file: {}", path.display()))?
+        .to_ascii_lowercase()
+        == "png";
+
+    if !is_png {
+        bail!("Image format not supported; PNGs are only supported.");
+    };
+
+    Ok(())
 }
 
 pub fn get_pixel_data(
-    sprite: &Sprite,
+    sprite: &ExpandedSprite,
     sprite_path: &PathBuf,
     metadata: &SpriteMetadata,
 ) -> anyhow::Result<(u32, u32, image::ImageBuffer<Rgb<u8>, Vec<u8>>)> {
-    let sprite = sprite.to_expanded();
-
     let source_image_path = sprite_path
         .parent()
         .with_context(|| "Sprite path was empty.")?
-        .join(sprite.path);
+        .join(sprite.path.to_path_buf());
 
-    {
-        let is_png = source_image_path
-            .extension()
-            .with_context(|| {
-                format!(
-                    "Failed to get extension of file: {}",
-                    source_image_path.display()
-                )
-            })?
-            .to_ascii_lowercase()
-            == "png";
-
-        if !is_png {
-            bail!("Image format not supported; PNGs are only supported.");
-        };
-    }
+    check_for_png(&source_image_path)?;
 
     let sprite_data = image::io::Reader::open(source_image_path.clone())?.decode()?;
 
@@ -187,22 +244,38 @@ pub fn generate_assembly_file(
     let mut output = String::new();
 
     for (sprite_suffix, sprite) in metadata.sprites.iter() {
-        let (width, height, pixels) = get_pixel_data(sprite, sprite_path, metadata)?;
+        let sprite = sprite.to_expanded();
+
+        let (width, height, pixels) = get_pixel_data(&sprite, sprite_path, metadata)?;
 
         let pixels = pixels
             .pixels()
             .map(|pixel| compress_color_space_rgb(pixel.0))
             .collect::<Vec<_>>();
 
-        generate_assembly_sprite(
-            &mut output,
-            RawSprite {
-                sprite_suffix,
-                width,
-                height,
-                pixels,
-            },
-        );
+        let color_space = unwrap_sprite_option(&metadata.color_space, &sprite.color_space);
+        let header = unwrap_sprite_option_or(&metadata.header, &sprite.header, true);
+
+        let raw_sprite = RawSprite {
+            sprite_suffix,
+            width,
+            height,
+            color_space,
+            pixels,
+        };
+
+        output += &format!("\n{}:", sprite_suffix);
+
+        if header {
+            generate_assembly_sprite_header(&mut output, &raw_sprite);
+        }
+
+        match raw_sprite.color_space {
+            ColorSpace::Rgb => generate_assembly_sprite_pixels_rgb(&mut output, &raw_sprite),
+            ColorSpace::Monochrome => {
+                generate_assembly_sprite_pixels_monochrome(&mut output, &raw_sprite)
+            }
+        }
     }
 
     let sprite_out = out_path.join(format!("{}.asm", sprite_collection_name));
@@ -223,23 +296,26 @@ pub fn generate_binary_file(
 ) -> anyhow::Result<()> {
     let mut output = Vec::with_capacity(64 * 1_024);
 
-    for (sprite_suffix, sprite) in metadata.sprites.iter() {
-        let (width, height, pixels) = get_pixel_data(sprite, sprite_path, metadata)?;
+    for sprite in metadata.sprites.values().map(Sprite::to_expanded) {
+        let (width, height, pixels) = get_pixel_data(&sprite, sprite_path, metadata)?;
 
-        let pixels = pixels
+        let mut pixels = pixels
             .pixels()
             .map(|pixel| compress_color_space_rgb(pixel.0))
             .collect::<Vec<_>>();
 
-        generate_binary_sprite(
-            &mut output,
-            RawSprite {
-                sprite_suffix,
-                width,
-                height,
-                pixels,
-            },
-        );
+        let color_space = unwrap_sprite_option(&metadata.color_space, &sprite.color_space);
+        let header = unwrap_sprite_option_or(&metadata.header, &sprite.header, true);
+
+        if header {
+            output.push(width as u8);
+            output.push(height as u8);
+        }
+
+        match color_space {
+            ColorSpace::Rgb => output.append(&mut pixels),
+            ColorSpace::Monochrome => bail!("Binary can't be used with monochrome."),
+        }
     }
 
     let sprite_out = out_path.join(format!("{}.bin", sprite_collection_name));
