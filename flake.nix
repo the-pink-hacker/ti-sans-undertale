563903d1194a16d0848621769d7f6400d6c9266d
Pink Garrett <pink@thepinkhacker.com>
2025-09-10 06:20:33 -0400

    Tweak flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,50 @@
 {
-    inputs.toolchain.url = "github:myclevorname/flake";
-
-    outputs = { self, toolchain }: {
-        packages.x86_64-linux.default = toolchain.packages.x86_64-linux.mkDerivation {
-            pname = "sans-ti";
-            version = "0.0.1";
-            src = self;
+    description = "The Sans Undertale boss fight for the TI-84+ CE";
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+        toolchain = {
+            url = "github:myclevorname/flake";
+            inputs = {
+                nixpkgs.follows = "nixpkgs";
+            };
         };
+        rust-overlay = {
+            url = "github:oxalica/rust-overlay";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+    outputs = {
+        self,
+        nixpkgs,
+        toolchain,
+        rust-overlay,
+        ...
+    }: let
+      inherit (nixpkgs) lib;
+      systems = [
+          "x86_64-linux"
+          "x86_64-darwin"
+      ];
+      pkgsFor = lib.genAttrs systems (system:
+          import nixpkgs {
+              localSystem.system = system;
+              overlays = [(import rust-overlay)];
+              config.allowUnfree = true;
+          });
+    in {
+        packages = lib.mapAttrs (system: pkgs: {
+            default = toolchain.packages.x86_64-linux.mkDerivation {
+                pname = "sans-ti";
+                version = "0.0.1";
+                src = self;
+            };
+        })
+        pkgsFor;
+        devShells = lib.mapAttrs (system: pkgs: {
+            default = pkgs.mkShell {
+                inputsFrom = [self.packages.${system}.default];
+            };
+        })
+        pkgsFor;
     };
 }
